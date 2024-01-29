c9449862ee6e4c2d5da3b1e7a0d1a13b34990dbd
arman <armanm.atc@gmail.com>
2024-01-30 00:19:43 +0330

    v 1.0.1

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,13 +1,33 @@
+
+
 require 'rails_helper'
 
-RSpec.describe Friend, type: :model do
-  it "can run tests" do
-    expect(true).to be(false)
-  end
-  describe "attributes" do
-    it { should respond_to(:first_name) }
-    it { should respond_to(:last_name) }
+
+
+  RSpec.describe Friend, type: :model do
+    let(:friend) { Friend.new(first_name: "my first post", last_name: "this is a body of my first post") }
+
+    it "is valid with a first name and last name" do
+      expect(friend).to be_valid
+    end
+
+    it "is invalid with a first name" do
+      post = Friend.new(first_name: nil)
+      post.valid?
+      expect(post.errors).to have_key(:first_name)
+
+    end
+
+    it "is invalid with a last name" do
+      post = Friend.new(last_name: nil)
+      post.valid?
+      expect(post.errors).to have_key(:last_name)
+
+    end
 
   end
 
-end
+
+
+
+
